60d51896197d7c6ae611401fc155bee86d663a03
Adrian Barrio <statickidz@gmail.com>
2024-10-02 11:37:51 +0200

    feat: refactor all

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 provider "oci" {}
 
 resource "oci_core_instance" "generated_oci_core_instance" {
-  availability_domain = var.availability_domain_2
+  display_name        = "coolify-main"
   compartment_id      = var.compartment_id
+  availability_domain = var.availability_domain_2
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
   create_vnic_details {
-    subnet_id = "${oci_core_subnet.generated_oci_core_subnet.id}"
+    subnet_id = oci_core_subnet.generated_oci_core_subnet.id
   }
   metadata = {
     "ssh_authorized_keys" = var.ssh_authorized_keys
@@ -17,14 +21,62 @@ resource "oci_core_instance" "generated_oci_core_instance" {
   source_details {
     source_id   = var.source_image_id
     source_type = "image"
+  }
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Management Agent"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Custom Logs Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute RDMA GPU Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Auto-Configuration"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Authentication"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Cloud Guard Workload Protection"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Block Volume Management"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
   }
 }
 
 resource "oci_core_instance" "generated_oci_worker_instance_1" {
-  availability_domain = var.availability_domain_3
+  display_name        = "coolify-worker-1"
   compartment_id      = var.compartment_id
+  availability_domain = var.availability_domain_3
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
   create_vnic_details {
-    subnet_id = "${oci_core_subnet.generated_oci_core_subnet.id}"
+    subnet_id = oci_core_subnet.generated_oci_core_subnet.id
   }
   metadata = {
     "ssh_authorized_keys" = var.ssh_authorized_keys
@@ -37,14 +89,62 @@ resource "oci_core_instance" "generated_oci_worker_instance_1" {
   source_details {
     source_id   = var.source_image_id
     source_type = "image"
+  }
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Management Agent"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Custom Logs Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute RDMA GPU Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Auto-Configuration"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Authentication"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Cloud Guard Workload Protection"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Block Volume Management"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
   }
 }
 
 resource "oci_core_instance" "generated_oci_worker_instance_2" {
-  availability_domain = var.availability_domain_1
+  display_name        = "coolify-worker-2"
   compartment_id      = var.compartment_id
+  availability_domain = var.availability_domain_1
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
   create_vnic_details {
-    subnet_id = "${oci_core_subnet.generated_oci_core_subnet.id}"
+    subnet_id = oci_core_subnet.generated_oci_core_subnet.id
   }
   metadata = {
     "ssh_authorized_keys" = var.ssh_authorized_keys
@@ -58,36 +158,80 @@ resource "oci_core_instance" "generated_oci_worker_instance_2" {
     source_id   = var.source_image_id
     source_type = "image"
   }
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Management Agent"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Custom Logs Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute RDMA GPU Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Auto-Configuration"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Authentication"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Cloud Guard Workload Protection"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Block Volume Management"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
+  }
 }
 
 resource "oci_core_vcn" "generated_oci_core_vcn" {
-  cidr_block       = "10.0.0.0/16"
-  compartment_id   = var.compartment_id
-  display_name     = "network-coolify"
-  dns_label        = "vcn09121104"
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = var.compartment_id
+  display_name   = "network-coolify"
+  dns_label      = "vcn09121104"
 }
 
 resource "oci_core_subnet" "generated_oci_core_subnet" {
-  cidr_block       = "10.0.0.0/24"
-  compartment_id   = var.compartment_id
-  display_name     = "subnet-coolify"
-  dns_label        = "subnet09121104"
-  route_table_id   = "${oci_core_vcn.generated_oci_core_vcn.default_route_table_id}"
-  vcn_id           = "${oci_core_vcn.generated_oci_core_vcn.id}"
+  cidr_block     = "10.0.0.0/24"
+  compartment_id = var.compartment_id
+  display_name   = "subnet-coolify"
+  dns_label      = "subnet09121104"
+  route_table_id = oci_core_vcn.generated_oci_core_vcn.default_route_table_id
+  vcn_id         = oci_core_vcn.generated_oci_core_vcn.id
 }
 
 resource "oci_core_internet_gateway" "generated_oci_core_internet_gateway" {
-  compartment_id   = var.compartment_id
-  display_name     = "Internet Gateway network-coolify"
-  enabled          = "true"
-  vcn_id           = "${oci_core_vcn.generated_oci_core_vcn.id}"
+  compartment_id = var.compartment_id
+  display_name   = "Internet Gateway network-coolify"
+  enabled        = "true"
+  vcn_id         = oci_core_vcn.generated_oci_core_vcn.id
 }
 
 resource "oci_core_default_route_table" "generated_oci_core_default_route_table" {
   route_rules {
-    destination        = "0.0.0.0/0"
-    destination_type   = "CIDR_BLOCK"
-    network_entity_id  = "${oci_core_internet_gateway.generated_oci_core_internet_gateway.id}"
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.generated_oci_core_internet_gateway.id
   }
-  manage_default_resource_id = "${oci_core_vcn.generated_oci_core_vcn.default_route_table_id}"
+  manage_default_resource_id = oci_core_vcn.generated_oci_core_vcn.default_route_table_id
 }
--- variables.tf
+++ variables.tf
@@ -26,23 +26,23 @@ variable "availability_domain_3" {
 variable "instance_shape" {
   description = "The shape of the instance."
   type        = string
-  default     = "VM.Standard.A1.Flex"
+  default     = "VM.Standard.A1.Flex" # OCI Free
 }
 
 variable "memory_in_gbs" {
   description = "Memory in GBs for instance shape config."
   type        = string
-  default     = "6"
+  default     = "6" # OCI Free
 }
 
 variable "ocpus" {
   description = "OCPUs for instance shape config."
   type        = string
-  default     = "1"
+  default     = "1" # OCI Free
 }
 
 variable "source_image_id" {
   description = "Source image OCID."
   type        = string
-  default     = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3rxaqcvwe2vxxffm4dfivmfb3apn4inqehxgntjrx3f7p4hzk5rq"
+  default     = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3rxaqcvwe2vxxffm4dfivmfb3apn4inqehxgntjrx3f7p4hzk5rq" # Ubuntu 20.04 LTS
 }
