242d3147f44c1c0459c58be2c4157538de0f333e
Adrian Barrio <statickidz@gmail.com>
2024-10-02 13:09:33 +0200

    feat: add ip outputs v2

--- main.tf
+++ main.tf
@@ -144,29 +144,17 @@ resource "oci_core_instance" "coolify_worker_2" {
   }
 }
 
-# Output the public and private IPs of the main instance
-output "coolify_main_private_ip" {
-  value = oci_core_instance.coolify_main.private_ip
+output "coolify_dashboard" {
+  value       = "http://${oci_core_instance.coolify_main.public_ip}:8000/"
+  description = "Coolify Dashboard URL"
 }
 
-output "coolify_main_public_ip" {
-  value = oci_core_instance.coolify_main.public_ip
+output "coolify_worker_1" {
+  value       = oci_core_instance.coolify_worker_1.public_ip
+  description = "coolify-worker-1 Public IP"
 }
 
-# Output the public and private IPs of the first worker instance
-output "coolify_worker_1_private_ip" {
-  value = oci_core_instance.coolify_worker_1.private_ip
-}
-
-output "coolify_worker_1_public_ip" {
-  value = oci_core_instance.coolify_worker_1.public_ip
-}
-
-# Output the public and private IPs of the second worker instance
-output "coolify_worker_2_private_ip" {
-  value = oci_core_instance.coolify_worker_2.private_ip
-}
-
-output "coolify_worker_2_public_ip" {
-  value = oci_core_instance.coolify_worker_2.public_ip
+output "coolify_worker_1" {
+  value       = oci_core_instance.coolify_worker_2.public_ip
+  description = "coolify-worker-2 Public IP"
 }
