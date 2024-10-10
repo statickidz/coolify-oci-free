6846988820f39498a819ffea3a91b2dcd29a2cfb
Adrian Barrio <statickidz@gmail.com>
2024-10-10 11:42:35 +0200

    feat: add worker selection

--- main.tf
+++ main.tf
@@ -21,11 +21,11 @@ locals {
   }
 }
 
-# Instance resources
+# Main instance
 resource "oci_core_instance" "coolify_main" {
   display_name        = "coolify-main"
   compartment_id      = var.compartment_id
-  availability_domain = var.availability_domain_1
+  availability_domain = var.availability_domain_main
 
   is_pv_encryption_in_transit_enabled = local.instance_config.is_pv_encryption_in_transit_enabled
   shape                               = local.instance_config.shape
@@ -61,13 +61,57 @@ resource "oci_core_instance" "coolify_main" {
     source_id   = local.instance_config.source_details.source_id
     source_type = local.instance_config.source_details.source_type
   }
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Management Agent"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Custom Logs Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute RDMA GPU Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Auto-Configuration"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Authentication"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Cloud Guard Workload Protection"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Block Volume Management"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
+  }
 }
 
 # Worker instances (similar to main instance)
-resource "oci_core_instance" "coolify_worker_1" {
-  display_name        = "coolify-worker-1"
-  compartment_id      = var.compartment_id
-  availability_domain = var.availability_domain_2
+resource "oci_core_instance" "coolify_worker" {
+  count               = var.num_worker_instances
+  display_name        = "coolify-worker-${count.index + 1}"
+  availability_domain = var.availability_domain_workers
 
   is_pv_encryption_in_transit_enabled = local.instance_config.is_pv_encryption_in_transit_enabled
   shape                               = local.instance_config.shape
@@ -103,87 +147,49 @@ resource "oci_core_instance" "coolify_worker_1" {
     source_id   = local.instance_config.source_details.source_id
     source_type = local.instance_config.source_details.source_type
   }
-}
-
-resource "oci_core_instance" "coolify_worker_2" {
-  display_name        = "coolify-worker-2"
-  compartment_id      = var.compartment_id
-  availability_domain = var.availability_domain_3
-
-  is_pv_encryption_in_transit_enabled = local.instance_config.is_pv_encryption_in_transit_enabled
-  shape                               = local.instance_config.shape
-
-  metadata = {
-    ssh_authorized_keys = local.instance_config.ssh_authorized_keys
-    user_data           = base64encode(file("./bin/deps.sh"))
-  }
-
-  create_vnic_details {
-    subnet_id = oci_core_subnet.coolify_subnet.id
-  }
-
   agent_config {
-    is_management_disabled = false
-    is_monitoring_disabled = false
-  }
-
-  availability_config {
-    recovery_action = local.instance_config.availability_config.recovery_action
-  }
-
-  instance_options {
-    are_legacy_imds_endpoints_disabled = local.instance_config.instance_options.are_legacy_imds_endpoints_disabled
-  }
-
-  shape_config {
-    memory_in_gbs = local.instance_config.shape_config.memory_in_gbs
-    ocpus         = local.instance_config.shape_config.ocpus
-  }
-
-  source_details {
-    source_id   = local.instance_config.source_details.source_id
-    source_type = local.instance_config.source_details.source_type
-  }
-}
-
-resource "oci_core_instance" "coolify_worker_3" {
-  display_name        = "coolify-worker-3"
-  compartment_id      = var.compartment_id
-  availability_domain = var.availability_domain_4
-
-  is_pv_encryption_in_transit_enabled = local.instance_config.is_pv_encryption_in_transit_enabled
-  shape                               = local.instance_config.shape
-
-  metadata = {
-    ssh_authorized_keys = local.instance_config.ssh_authorized_keys
-    user_data           = base64encode(file("./bin/deps.sh"))
-  }
-
-  create_vnic_details {
-    subnet_id = oci_core_subnet.coolify_subnet.id
-  }
-
-  agent_config {
-    is_management_disabled = false
-    is_monitoring_disabled = false
-  }
-
-  availability_config {
-    recovery_action = local.instance_config.availability_config.recovery_action
-  }
-
-  instance_options {
-    are_legacy_imds_endpoints_disabled = local.instance_config.instance_options.are_legacy_imds_endpoints_disabled
-  }
-
-  shape_config {
-    memory_in_gbs = local.instance_config.shape_config.memory_in_gbs
-    ocpus         = local.instance_config.shape_config.ocpus
-  }
-
-  source_details {
-    source_id   = local.instance_config.source_details.source_id
-    source_type = local.instance_config.source_details.source_type
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Management Agent"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Custom Logs Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute RDMA GPU Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Auto-Configuration"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Authentication"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Cloud Guard Workload Protection"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Block Volume Management"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
   }
 }
 
@@ -191,14 +197,6 @@ output "coolify_dashboard" {
   value = "http://${oci_core_instance.coolify_main.public_ip}:8000/ (wait 2-3 minutes to finish Coolify installation)"
 }
 
-output "coolify_worker_1" {
-  value = "${oci_core_instance.coolify_worker_1.public_ip} (user it to add the server in Coolify Dashboard)"
-}
-
-output "coolify_worker_2" {
-  value = "${oci_core_instance.coolify_worker_2.public_ip} (user it to add the server in Coolify Dashboard)"
-}
-
-output "coolify_worker_3" {
-  value = "${oci_core_instance.coolify_worker_3.public_ip} (user it to add the server in Coolify Dashboard)"
+output "coolify_worker_ips" {
+  value = [for instance in oci_core_instance.coolify_worker : "${instance.public_ip} (user it to add the server in Coolify Dashboard)"]
 }
--- variables.tf
+++ variables.tf
@@ -8,40 +8,36 @@ variable "compartment_id" {
   type        = string
 }
 
-variable "availability_domain_1" {
+variable "num_worker_instances" {
+  description = "Number of Coolify worker instances to deploy."
+  type        = number
+  default     = 3 # Default number of worker instances
+}
+
+variable "availability_domain_main" {
   description = "Availability domain for coolify-main instance. For example: AaJg:EU-FRANKFURT-1-AD-1"
   type        = string
 }
 
-variable "availability_domain_2" {
-  description = "Availability domain for coolify-worker-1 instance. For example: AaJg:EU-FRANKFURT-1-AD-1"
-  type        = string
-}
-
-variable "availability_domain_3" {
-  description = "Availability domain for coolify-worker-2 instance. For example: AaJg:EU-FRANKFURT-1-AD-1"
-  type        = string
-}
-
-variable "availability_domain_4" {
-  description = "Availability domain for coolify-worker-3 instance. For example: AaJg:EU-FRANKFURT-1-AD-1"
+variable "availability_domain_workers" {
+  description = "Availability domain for coolify-main instance. For example: AaJg:EU-FRANKFURT-1-AD-2"
   type        = string
 }
 
 variable "instance_shape" {
-  description = "The shape of the instance."
+  description = "The shape of the instance. VM.Standard.A1.Flex is free tier eligible."
   type        = string
   default     = "VM.Standard.A1.Flex" # OCI Free
 }
 
 variable "memory_in_gbs" {
-  description = "Memory in GBs for instance shape config."
+  description = "Memory in GBs for instance shape config. 6 GB is the maximum for free tier with 3 working nodes."
   type        = string
   default     = "6" # OCI Free
 }
 
 variable "ocpus" {
-  description = "OCPUs for instance shape config."
+  description = "OCPUs for instance shape config. 1 OCPU is the maximum for free tier with 3 working nodes."
   type        = string
   default     = "1" # OCI Free
 }
