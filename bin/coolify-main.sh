e2e7548807fb6d5fe1a1f871c853cde4091f6123
Adrian Barrio <statickidz@gmail.com>
2024-10-13 11:47:59 +0200

    chore: comment build install

--- bin/coolify-main.sh
+++ bin/coolify-main.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Add ubuntu to sudoers
-echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+# # Add ubuntu to sudoers
+# echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-# OpenSSH
-sudo apt install openssh-server
-sudo systemctl status sshd
+# # OpenSSH
+# sudo apt install openssh-server
+# sudo systemctl status sshd
 
-# Permit root login
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# # Permit root login
+# sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# sudo systemctl restart sshd
 
-# Install Coolify
-sudo curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
+# # Install Coolify
+# sudo curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
