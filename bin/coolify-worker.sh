7ae7a58cb699c9659ab4c1f874462dedbd56d76a
Adrian Barrio <statickidz@gmail.com>
2024-10-13 13:12:53 +0200

    feat: add cloud init config

--- bin/coolify-worker.sh
+++ bin/coolify-worker.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Define the cloud.cfg path
+CLOUD_CFG="/etc/cloud/cloud.cfg"
+
+# Modify disable_root to allow root login
+sudo sed -i 's/^disable_root: true/disable_root: false/' $CLOUD_CFG
+
+# Ensure ssh_pwauth is enabled
+if grep -q "^ssh_pwauth" $CLOUD_CFG; then
+    sudo sed -i 's/^ssh_pwauth: .*/ssh_pwauth: true/' $CLOUD_CFG
+else
+    echo "ssh_pwauth: true" | sudo tee -a $CLOUD_CFG
+fi
+
 # Add ubuntu to sudoers
 echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
