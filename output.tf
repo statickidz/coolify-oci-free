3b9c394c0473d9da8e5b9d694882e59829b6eb63
Adrian Barrio <statickidz@gmail.com>
2024-10-15 11:19:40 +0200

    chore: add output tf

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "coolify_dashboard" {
+  value = "http://${oci_core_instance.coolify_main.public_ip}:8000/ (wait 3-5 minutes to finish Coolify installation)"
+}
+
+output "coolify_worker_ips" {
+  value = [for instance in oci_core_instance.coolify_worker : "${instance.public_ip} (user it to add the server in Coolify Dashboard)"]
+}
