b513ab5d0bf2ee19367e2b9f47abb4c9c69d79b8
Adrian Barrio <statickidz@gmail.com>
2024-10-18 08:50:58 +0200

    fix: output typo

--- output.tf
+++ output.tf
@@ -3,5 +3,5 @@ output "coolify_dashboard" {
 }
 
 output "coolify_worker_ips" {
-  value = [for instance in oci_core_instance.coolify_worker : "${instance.public_ip} (user it to add the server in Coolify Dashboard)"]
+  value = [for instance in oci_core_instance.coolify_worker : "${instance.public_ip} (use it to add the server in Coolify Dashboard)"]
 }
